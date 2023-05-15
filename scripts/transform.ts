6ab084986873fd7f3680443a4ff910e5658fce26
SoloJiang <350740249@qq.com>
2023-05-15 10:33:27 +0800

    feat: init project transformer

---
+++ scripts/transform.ts
@@ -0,0 +1,57 @@
+import * as glob from 'glob';
+import esbuild from 'esbuild';
+import { parseArgs } from 'node:util';
+import { watch } from 'chokidar';
+import { join } from 'path';
+
+const cwd = process.cwd();
+const SRC_DIR = 'src';
+const DIST_DIR = 'dist';
+const args = ['-w'];
+const parseArgsOptions = {
+  watch: {
+    type: 'boolean' as const,
+    short: 'w',
+    default: false,
+  },
+};
+
+function genResultFile(path: string | string[]) {
+  return esbuild.build({
+    entryPoints: Array.isArray(path) ? path : [path],
+    outdir: DIST_DIR,
+    platform: 'node',
+  });
+}
+
+function updateResultFile(path: string) {
+  console.log(`${path} changed.\n`);
+  genResultFile(path)
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function transform() {
+  const files: string[] = glob.sync(`${SRC_DIR}/**`, { nodir: true, ignore: 'node_modules/**' });
+  const { values: commandArgs } = parseArgs({
+    args,
+    options: parseArgsOptions,
+  });
+  genResultFile(files);
+
+  // Watch mode
+  if (commandArgs.watch) {
+    const watcher = watch(join(cwd, SRC_DIR), {
+      ignoreInitial: true,
+      awaitWriteFinish: {
+        stabilityThreshold: 600,
+      },
+    });
+
+    console.log('Watching for file changes...\n');
+    watcher.on('add', updateResultFile).on('change', updateResultFile);
+  }
+}
+
+transform();
